2617a632f9e030617475967e4ca0d487e1c38734
Hamza-Alnour <hamzaalnoour@gmail.com>
2023-05-07 23:28:50 +0300

    Api

--- SWE_206_FX/SWE_206/src/main/java/module-info.java
+++ SWE_206_FX/SWE_206/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.swe_206 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
 
 
     opens com.example.swe_206 to javafx.fxml;
